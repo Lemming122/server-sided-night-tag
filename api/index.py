2bd25bfbb8a189dd43cf01a4d8c553963f012bdc
Lemming122 <kay550733@gmail.com>
2026-05-21 00:02:25 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,62 @@
-from flask import Flask
+function CySided(args, type = null) {
+    var newSS = true;
 
-app = Flask(__name__)
+    const SharedGroupId = args.GameId + args.Region.toUpperCase();
+    
+    let concatItems = GetPlayerInventory(currentPlayerId);
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    var id = newSS ? args.ActorNr.toString() : currentPlayerId;
 
-@app.route('/about')
-def about():
-    return 'About'
+    if (args.Type == "Create") {
+        server.CreateSharedGroup({
+            SharedGroupId: SharedGroupId
+        });
+        server.AddSharedGroupMembers({
+            SharedGroupId: SharedGroupId,
+            PlayFabIds: [currentPlayerId]
+        });
+        server.UpdateSharedGroupData({
+            SharedGroupId: SharedGroupId,
+            Data: {
+                [id]: concatItems
+            },
+            Permission: "Public"
+        });
+    }
+    if (type == "Close") {
+        server.DeleteSharedGroup({
+            SharedGroupId: SharedGroupId
+        });
+    }
+    if (args.Type == "Join") {
+        server.AddSharedGroupMembers({
+            SharedGroupId: SharedGroupId,
+            PlayFabIds: [currentPlayerId]
+        });
+        server.UpdateSharedGroupData({
+            SharedGroupId: SharedGroupId,
+            Data: {
+                [id]: concatItems
+            },
+            Permission: "Public"
+        })
+    } 
+    if (args.Type == "ClientDisconnect" || args.Type == "TimeoutDisconnect") {
+        server.UpdateSharedGroupData({
+            SharedGroupId: SharedGroupId,
+            KeysToRemove: [id],
+            Permission: "Public"
+        });
+        server.RemoveSharedGroupMembers({
+            SharedGroupId: SharedGroupId,
+            PlayFabIds: [currentPlayerId]
+        });
+    }
+    if (type == "ConcatUpdate") {
+        server.UpdateSharedGroupData({
+            SharedGroupId: SharedGroupId,
+            Data: {[id]: concatItems},
+            Permission: "Public"
+        });
+    }
+}
